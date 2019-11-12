abb391d59d07bc548ba4849c3481091ecc8606e1
Misha Ward <wardm5@uw.edu>
2019-11-12 11:37:03 -0800

    Update shapeArea.java

--- Arcade/Intro/shapeArea.java
+++ Arcade/Intro/shapeArea.java
@@ -1,6 +1,11 @@
 int shapeArea(int n) {
-    if (n == 1)
-        return 1;
-    // System.out.println((n*n) + "   " + (n-2) +   "    ");
-    return ((n*2) + ((n-2) * 2)) + shapeArea(n-1);
+    // if (n == 1)
+    //     return 1;
+    // // System.out.println((n*n) + "   " + (n-2) +   "    ");
+    // return ((n*2) + ((n-2) * 2)) + shapeArea(n-1);
+    int sum = 1;
+    for (int i = 2; i <= n; i++) {
+        sum += (i * 2) + ((i - 2)  * 2);
+    }
+    return sum;
 }
