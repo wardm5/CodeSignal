6980e34aafeb90ba721cd7681ce8cd61460c3275
Misha Ward <wardm5@uw.edu>
2019-11-12 11:08:18 -0800

    Update centuryFromYear.java

--- Arcade/Intro/centuryFromYear.java
+++ Arcade/Intro/centuryFromYear.java
@@ -1,12 +1,15 @@
 int centuryFromYear(int year) {
-    String s = year + "";
-    if (s.length() <= 2)
-        return 1;
-    if (s.length() <= 3 && Integer.valueOf(s.substring(s.length()-2,s.length())) > 0)
-        return Integer.valueOf(s.substring(0,1)) + 1;
-    else if (s.length() <= 3)
-        return Integer.valueOf(s.substring(0,1));
-    if (Integer.valueOf(s.substring(s.length()-2,s.length())) > 0)
-        return Integer.valueOf(s.substring(0,2)) + 1;
-    return Integer.valueOf(s.substring(0,2));
+//     String s = year + "";
+//     if (s.length() <= 2)
+//         return 1;
+//     if (s.length() <= 3 && Integer.valueOf(s.substring(s.length()-2,s.length())) > 0)
+//         return Integer.valueOf(s.substring(0,1)) + 1;
+//     else if (s.length() <= 3)
+//         return Integer.valueOf(s.substring(0,1));
+//     if (Integer.valueOf(s.substring(s.length()-2,s.length())) > 0)
+//         return Integer.valueOf(s.substring(0,2)) + 1;
+//     return Integer.valueOf(s.substring(0,2));
+// }
+
+    return 1 + (year - 1) / 100;
 }
