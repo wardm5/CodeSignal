6d0bf70f9b70d4a8b318115459d9bbbb4da88885
Misha Ward <wardm5@uw.edu>
2019-11-12 11:47:41 -0800

    Create makeArrayConsecutive2.java

---
+++ Arcade/Intro/makeArrayConsecutive2.java
@@ -0,0 +1,18 @@
+int makeArrayConsecutive2(int[] statues) {
+    TreeSet<Integer> set = new TreeSet<>();
+    for (int i = 0; i < statues.length; i++) {
+        set.add(statues[i]);
+    }
+    int count = 0; 
+    int prior = -1;
+    for (int statue : set) {
+        if (prior == -1) {
+            prior = statue;
+            continue;
+        } 
+        if (statue - prior > 1)
+            count += statue - prior - 1;
+        prior = statue;
+    }
+    return count;
+}
